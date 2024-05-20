75c3c2d6608f0df7d08059cbc2f40512ceea42c8
kevinelong <kevinelong@gmail.com>
2024-05-20 10:57:51 -0700

    update

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
     // mountainsArray.forEach(m => mountainList.appendChild(option(m.name)));
-
     for (mountain of mountainsArray) {
         const name = mountain.name;
         const o = option(name)
@@ -11,20 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     mountainList.addEventListener("change", e => {
         const mountain = mountainsArray[mountainList.selectedIndex - 1];
-        // coords: {
-        //     lat: 44.270403,
-        //     lng: -71.303501
-        // }
         mountainResults.innerHTML = `
-        <br>
-        <img src="./images/${mountain.img}">
-
-        <h3>${mountain.name}</h3>
-        Elevation: ${mountain.elevation} feet<br>
-        Effort: ${mountain.effort}<br>
-        Lattitude/Longitude: (${mountain.coords.lat}, ${mountain.coords.lng})
-        <p> ${mountain.desc} </p>
-    `;
+            <br>
+            <img src="./images/${mountain.img}">
+            <h3>${mountain.name}</h3>
+            Elevation: ${mountain.elevation} feet<br>
+            Effort: ${mountain.effort}<br>
+            Lattitude/Longitude: (${mountain.coords.lat}, ${mountain.coords.lng})
+            <p> ${mountain.desc} </p>
+        `;
     });//end change
 
 });//end loaded
